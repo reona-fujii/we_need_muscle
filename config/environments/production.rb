f181c47061df82b8918bfbcaea0ce92c2cc5a557
EC2 Default User <ec2-user@ip-172-31-44-234.ap-northeast-1.compute.internal>
2020-11-24 05:33:15 +0000

    deploy3

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -20,7 +20,7 @@ Rails.application.configure do
 
   # Disable serving static files from the `/public` folder by default since
   # Apache or NGINX already handles this.
-  # config.public_file_server.enabled = ENV['RAILS_SERVE_STATIC_FILES'].present?
+  # config.public_file_server.enabled = ENV['RAILS_SERVE_STATIC_FILES'].present?（css反映されないため以下に変更）
   config.public_file_server.enabled = true
   # Compress JavaScripts and CSS.
   # config.assets.js_compressor = :uglifier(本番立ち上げ時にエラーが出たため以下に変更)
@@ -28,8 +28,8 @@ Rails.application.configure do
   # config.assets.css_compressor = :sass
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
-  config.assets.compile = false
-
+  # config.assets.compile = false(css反映されないため以下に変更)
+  config.assets.compile = true
   # `config.assets.precompile` and `config.assets.version` have moved to config/initializers/assets.rb
 
   # Enable serving of images, stylesheets, and JavaScripts from an asset server.
